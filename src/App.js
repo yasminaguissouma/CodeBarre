024f8eeee54198189b6632a4ffc88bd24c73e86e
Yassou <yasmina.guissouma@amadeus.com>
2017-01-05 11:01:09 +0000

    first form style ko

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const BarCodeForm = require('./barcodeform').default;
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Impression Etiquettes Clients</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
         </p>
+          <BarCodeForm> </BarCodeForm>
+
+
       </div>
     );
   }
---
+++ src/barcodeform.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Card, Col, Row, Icon,Input, Button } from 'react-materialize';
+
+/**
+ * barcodeform component
+ */
+let BarCodeForm = React.createClass({
+    getInitialState: function(){
+        return {
+            salut:"salut"
+        }
+    },
+    render() {
+        return(
+            <Col s={12} m={6} l={4}>
+                <Card>
+                    <Row className={"text-blue"} >
+                        <Input className={"text-blue"} s={12} label="Prenom" validate><Icon>account_circle</Icon></Input>
+                        <Input className={"text-blue"} s={12} label="Nom" validate><Icon>account_circle</Icon></Input>
+                        <Input className={"text-blue"} s={12} label="Date de Naissance" validate type='tel'><Icon>phone</Icon></Input>
+                        <Input className={"text-blue"} s={12} label="Reference" validate type='email'><Icon>email</Icon></Input>
+                        <Button className={"text-blue"} type='submit'>Imprimer</Button>
+
+                    </Row>
+                </Card>
+            </Col>
+        );
+    }
+});
+
+export default BarCodeForm;
